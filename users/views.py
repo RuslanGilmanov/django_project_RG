986528320b6c1ca285c8b21d63a42ae6df727f45
Ruslan Gilmanov <r_gilmanov2005@hotmail.com>
2023-07-03 22:28:53 +1000

    Replaced get_user_model().DoesNotExist with ObjectDoesNotExist in users/views.py

--- users/views.py
+++ users/views.py
@@ -10,6 +10,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from .tokens import account_activation_token
 from django.template.loader import render_to_string
 from django.core.mail import EmailMessage
+from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -64,7 +65,7 @@ def activate(request, uidb64, token):
     try:
         uid = force_str(urlsafe_base64_decode(uidb64))
         user = get_user_model().objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, get_user_model().DoesNotExist):
+    except (TypeError, ValueError, OverflowError, ObjectDoesNotExist):
         user = None
 
     if user is not None and account_activation_token.check_token(user, token):
